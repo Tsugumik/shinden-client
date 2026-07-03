60e106ed4142bd37396b56a859178b2f09ff062e
Błażej Drozd <59662074+Tsugumik@users.noreply.github.com>
2026-07-03 11:25:36 +0200

    Project template

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NavBar from '@renderer/components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar />
+</template>
---
+++ src/renderer/src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="font-[Orbitron]">Shinden Client 5</header>
+</template>
+
+<style scoped></style>
